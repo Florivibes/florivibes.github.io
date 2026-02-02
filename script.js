2e3c9af84b85ae2a4597e9f22b84c88e5df834ec
Florivibes <jess.mcht@gmail.com>
2026-02-02 20:30:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,44 +8,49 @@ function openPopup(type) {
     evenementiel: `
       <div class="popup-art">
         <img src="img/evenementiel.jpg" alt="Événementiel" class="popup-art-img">
+        <h4>Formation Événementiel</h4>
+        <p>Créer, organiser et gérer des événements professionnels ou privés.</p>
 
-        <h4>Organisation événementielle</h4>
-        <p><strong>Durée :</strong> 35 heures (5 jours)</p>
-        <p><strong>Format :</strong> Présentiel ou distanciel</p>
-        <p><strong>Public visé :</strong> Adultes en reconversion, salariés, entrepreneurs</p>
-        <p><strong>Prérequis :</strong> Aucun</p>
-        <p><strong>Objectifs :</strong></p>
-        <ul>
-          <li>Analyser un brief client</li>
-          <li>Concevoir un événement professionnel</li>
-          <li>Établir un budget et un planning</li>
-          <li>Coordonner les prestataires</li>
-          <li>Anticiper les risques</li>
-        </ul>
+        <div class="popup-actions">
+          <button class="btn" onclick="window.location.href='formations.html'">Voir les formations</button>
+          <button class="btn" onclick="window.location.href='brochure.html'">Demandez la brochure !</button>
+        </div>
       </div>
     `,
     bienetre: `
       <div class="popup-art">
         <img src="img/bienetre.jpg" alt="Bien-être" class="popup-art-img">
+        <h4>Formations Beauté & Bien-être</h4>
+        <p>Soins, relaxation, esthétique et développement personnel.</p>
 
-        <h4>Beauté / Bien-être</h4>
-        <p>Formations pour le bien-être, soins, relaxation et développement personnel.</p>
+        <div class="popup-actions">
+          <button class="btn" onclick="window.location.href='formations.html'">Voir les formations</button>
+          <button class="btn" onclick="window.location.href='brochure.html'">Demandez la brochure !</button>
+        </div>
       </div>
     `,
     art: `
       <div class="popup-art">
-        <img src="img/art-evenementiel.jpg" alt="Art et événementiel" class="popup-art-img">
+        <img src="img/art-evenementiel.jpg" alt="Art et cinéma" class="popup-art-img">
+        <h4>Formations des Arts et du Cinéma</h4>
+        <p>Création, scène, cinéma, expression artistique et storytelling.</p>
 
-        <h4>Art</h4>
-        <p>Formations artistiques, musique, scène, expression créative.</p>
+        <div class="popup-actions">
+          <button class="btn" onclick="window.location.href='formations.html'">Voir les formations</button>
+          <button class="btn" onclick="window.location.href='brochure.html'">Demandez la brochure !</button>
+        </div>
       </div>
     `,
     management: `
       <div class="popup-art">
         <img src="img/management.jpg" alt="Management" class="popup-art-img">
-
         <h4>Coaching & Management</h4>
-        <p>Formations en leadership, management, coaching et gestion d’équipe.</p>
+        <p>Leadership, gestion d’équipe, coaching et développement professionnel.</p>
+
+        <div class="popup-actions">
+          <button class="btn" onclick="window.location.href='formations.html'">Voir les formations</button>
+          <button class="btn" onclick="window.location.href='brochure.html'">Demandez la brochure !</button>
+        </div>
       </div>
     `
   };
@@ -65,3 +70,4 @@ document.getElementById("popup").addEventListener("click", (e) => {
     closePopup();
   }
 });
+
